4023a1a25a121dc31cd4039211d75e89b759ab15
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-11 21:29:32 +0300

    create model placebet

--- betsite/models.py
+++ betsite/models.py
@@ -6,7 +6,6 @@ class Deposit(models.Model):
     id_no = models.CharField(max_length=60)
     mobile_number = models.CharField(max_length=20)
     amount = models.CharField(max_length=20)
-    admin = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -25,4 +24,28 @@ class Deposit(models.Model):
     @classmethod
     def update_deposit(cls,id):
         updated = Deposit.objects.filter(id=Deposit.id).update(name=name)
+        return updated
+
+class Placebet(models.Model):
+    game_id = models.CharField(max_length=60)
+    amount = models.CharField(max_length=20)
+    prediction = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def save_placebet(self):
+        self.save()
+
+    def delete_placebet(self):
+        self.delete()
+
+    @classmethod
+    def get_placebet(cls):
+        bet = Placebet.objects.all()
+        return bet
+
+    @classmethod
+    def update_placebet(cls,id):
+        updated = Placebet.objects.filter(id=Placebet.id).update(name=name)
         return updated
