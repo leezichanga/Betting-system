31e81068ddf5fbc605142e4c9cdc89314b1f5316
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-11 21:21:34 +0300

    create model deposit

--- betsite/models.py
+++ betsite/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Deposit(models.Model):
+    id_no = models.CharField(max_length=60)
+    mobile_number = models.CharField(max_length=20)
+    amount = models.CharField(max_length=20)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_deposit(self):
+        self.save()
+
+    def delete_deposit(self):
+        self.delete()
+
+    @classmethod
+    def get_deposit(cls):
+        deposit = Deposit.objects.all()
+        return deposit
+
+    @classmethod
+    def update_deposit(cls,id):
+        updated = Deposit.objects.filter(id=Deposit.id).update(name=name)
+        return updated
