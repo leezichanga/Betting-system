7b4d088e875c2753613fa387f5896f347fb3add6
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-12 15:53:45 +0300

    final code

--- betsite/models.py
+++ betsite/models.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 class Deposit(models.Model):
     name =  models.CharField(max_length=60)
     id_no = models.CharField(max_length=60)
-    email = models.CharField(max_length=60)
     mobile_number = models.CharField(max_length=20)
     amount = models.CharField(max_length=20)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
 
     def __str__(self):
         return self.name
@@ -32,7 +31,7 @@ class Deposit(models.Model):
 class Placebet(models.Model):
     game_id = models.CharField(max_length=60)
     amount = models.CharField(max_length=20)
-    prediction = models.CharField(max_length=20)
+    prediction = models.BooleanField(default=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
